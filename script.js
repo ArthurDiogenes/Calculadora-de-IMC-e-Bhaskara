85fe3832d5fa605463d8b64cd88173aa1155e83a
Arthur Diógenes <arthurdfleite@gmail.com>
2025-04-24 09:21:37 -0300

    fix

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-//IMC
+// IMC
 
-let peso = parseInt(147);
-let altura = parseFloat(1.84);
+let peso = 147;
+let altura = 1.84;
 
-let imc = peso / (altura^2);
+let imc = peso / (altura * altura);
+console.log(`Seu IMC é ${imc.toFixed(2)}`);
 
-console.log(`Seu IMC é ${imc}`);
+console.log("Abaixo de 18.5: Abaixo do peso");
+console.log("18.5 a 24.9: Peso normal");
+console.log("25 a 29.9: Sobrepeso");
+console.log("30 a 34.9: Obesidade grau 1");
+console.log("35 a 39.9: Obesidade grau 2");
+console.log("40 ou mais: Obesidade grau 3 (mórbida)");
+
 
 //Bhaskara
 
-let a = parseInt(prompt("Coeficiente 1:"));
-let b = parseInt(prompt("Coeficiente 2:"));
-let c = parseInt(prompt("Coeficiente 3:"));
+let a = parseFloat(prompt("Coeficiente a:"));
+let b = parseFloat(prompt("Coeficiente b:"));
+let c = parseFloat(prompt("Coeficiente c:"));
 
-let delta = (b*2) - (4*a*c);
-let x1 = Number((-b+(Math.sqrt(delta)))/(2*a));
-let x2 = Number((-b-(Math.sqrt(delta)))/(2*a));
+let delta = (b * b) - (4 * a * c);
 
-console.log(x1);
-console.log(x2);
+let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+console.log(`x1 = ${x1}`);
+console.log(`x2 = ${x2}`);
